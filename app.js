8b343a644508f40b011039ffd0b06468b4ea933c
Javed <javedahmednizamani@gmail.com>
2023-10-17 23:40:38 +0500

    weather api

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const request = require('request');
+const bodyParser = require('body-parser');
 const app = express();
+var url = 'https://samples.openweathermap.org/data/2.5/forecast?q=${city}';
+var appid = 'appid=47f2a111133960a747e1562330e688f7'
 
 
 app.get('/',(req, res)=>{
+    
     let city = req.query.city;
     const request = require('request');
-request(`https://samples.openweathermap.org/data/2.5/forecast?q=${city}&appid=47f2a111133960a747e1562330e688f7`, function (error, response, body) {
+    url = url+city+"&"+appid;
+request(url, function (error, response, body) {
     if(response.statusCode === 200){
         let data = JSON.parse(body);
         res.send(`The weather in your City  ${city} is ${data.list[0].weather[0].description}`);
